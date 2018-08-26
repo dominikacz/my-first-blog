d6449e596eaeb049ca8fdcd1c64bec14ed1c0244
dominikacz <00dominikacz@gmail.com>
2018-08-26 10:56:33 +0200

    My Django Girls app

---
+++ sobota.py
@@ -0,0 +1,54 @@
+print('Hello, Dominika')
+a = 6
+b = 10
+c = a + b
+print(c)
+
+
+
+
+if 3>2:
+    print('To działa')
+#
+
+
+
+#
+#
+# if 5>2:
+#     print('5 nie jest jednak więsze od 2')
+# elif 5==2:
+#     print('5 nie jest większe od 2')
+# name = 'Domi'
+# if name == 'Ola':
+    print('Hej Ola!')
+elif name == 'Domi' :
+    print('Hej Domi!')
+else:
+    print('Hej you')
+
+
+volume = 57
+
+
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20<= volume < 40:
+    print("It's nice for background music")
+elif 40<= volume < 60:
+    print ("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print ("Nice for parties")
+elif 80 <= volume < 100:
+    print ("A bit loud!")
+else:
+    print("My ears are hurting! :(")
+#Komenatarz
+if volume <20 or volume > 80:
+    volume = 50
+    print ("That's better!")
+
+range(5)
+
+for i in range (0,301,50):
+    print(i)
